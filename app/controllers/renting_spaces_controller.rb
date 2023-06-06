d40c5c2c4eee664bf73d1be80d84c95a20279327
Divya Savai <divyasam1027@gmail.com>
2023-06-06 13:20:03 +0200

    Added actions to the controllers

--- app/controllers/renting_spaces_controller.rb
+++ app/controllers/renting_spaces_controller.rb
@@ -1,38 +1,42 @@
 class RentingSpacesController < ApplicationController
+  before_action :set_renting_space, only: [:show, :edit, :update, :destroy]
+
   def index
     @renting_spaces = RentingSpace.all
   end
 
-  # def show
-  #   @renting_space = RentingSpace.find(params[:id])
-  # end
+  def show
+  end
 
-  # def new
-  #   @renting_space = RentingSpace.new
-  # end
+  def new
+    @user = current_user
+    @renting_space = RentingSpace.new
+  end
 
-  # def create
-  #   @renting_space = RentingSpace.new(rentingspaces_params)
-  #   @renting_space.save
-  # end
+  def create
+    @renting_space = RentingSpace.new(rentingspaces_params)
+    @renting_space.user = current_user
+    @renting_space.save
+  end
 
-  # def edit
-  #   @renting_space = RentingSpace.find(:id)
-  # end
+  def edit
+  end
 
-  # def update
-  #   @renting_space = RentingSpace.find(:id)
-  #   @renting_space.update
-  # end
+  def update
+    @renting_space.update(params[:renting_space])
+  end
 
-  # def destroy
-  #   @renting_space = RentingSpace.find(:id)
-  #   @renting_space.destroy
-  # end
+  def destroy
+    @renting_space.destroy
+  end
 
-  # private
+  private
 
-  # def rentingspaces_params
-  #   params.require(:renting_space).permit(:name, :address, :aminities, :internet, :user_id)
-  # end
+  def set_renting_space
+    @renting_space = RentingSpace.find(params[:id])
+  end
+
+  def rentingspaces_params
+    params.require(:renting_space).permit(:name)
+  end
 end
